b4bf4c7c32c08f964c1f7e87a2d72e39477f04e4
peelz <peelz.dev@gmail.com>
2022-09-13 20:02:09 -0400

    Fix cast exception when calling a method that takes a nullable enum

---
+++ MoonSharp.Interpreter.Tests/EndToEnd/ScriptToClrTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace MoonSharp.Interpreter.Tests.EndToEnd
+{
+	[TestFixture]
+	public class ScriptToClrTests
+	{
+		private enum MyEnum
+		{
+			A = 1,
+		}
+
+		private class EnumTest
+		{
+			public void TakesNullableEnum(MyEnum? myEnum) { }
+		}
+
+		[Test]
+		public void TakesNullableEnum()
+		{
+			var s = new Script();
+			UserData.RegisterType<EnumTest>(InteropAccessMode.Reflection);
+			s.Globals["Test"] = UserData.CreateStatic<EnumTest>();
+			s.DoString("Test.__new().TakesNullableEnum(1)");
+		}
+	}
+}
